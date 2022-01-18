ddb927b3ebb2d05055c08d9838620eef04d36e6c
franturles <kikofran35@hotmail.com>
2022-01-18 08:44:05 +0000

    Ejercicio 5

---
+++ ejercicios_5.php
@@ -0,0 +1,16 @@
+<html>
+
+<?php
+$x=144;
+$y=999;
+$Suma = $x+$y;
+$Resta = $x-$y;
+$Multiplicacion = $x*$y;
+$Division = $y/$x;
+
+echo "$x + $y = $Suma" ."</br>";
+echo "$x - $y = $Resta" ."</br>";
+echo "$x x $y = $Multiplicacion" ."</br>";
+echo "$y / $x = $Division" ."</br>";
+?>
+</html>
