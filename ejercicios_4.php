239241812319775d5a075b463293ad21221e746c
franturles <kikofran35@hotmail.com>
2022-01-18 08:30:36 +0000

    Ejercicio 4

--- ejercicios_4.php
+++ ejercicios_4.php
@@ -12,84 +12,105 @@
         </thead>
         <tbody>
             <tr>
-                <td>8:50-9:40</td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
+                <?php
+                echo "<td>8:50-9:40</td> 
+                <td>IAW</td>
+                <td>SAD</td>
+                <td>ASX</td>
+                <td>ASX</td>
+                <td>IAW</td>"
+                ?>
             </tr>
             <tr>
-                <td>9:40-10:30</td>
-                <td>Hola</td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
+                <?php 
+                 echo "<td>9:40-10:30</td>
+                <td>IAW</td>
+                <td>SAD</td>
+                <td>EIEA</td>
+                <td>ASX</td>
+                <td>SRI</td>"
+                ?>
             </tr>
             <tr>
-                <td>10:30-11:20</td>
-                <td>Donde</td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
+                <?php 
+                echo "<td>10:30-11:20</td>
+                <td>SRI</td>
+                <td>SAD</td>
+                <td>EIEA</td>
+                <td>EIEA</td>
+                <td>SRI</td>"
+                ?>
             </tr>
             <tr>
-                <td>11:20-12:00</td>
-                <td>Que</td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
+                <?php 
+                echo "<td>11:20-12:00</td>"
+                ?>
+                <td colspan="5" align="Center">RECREO</td>
             </tr>
             <tr>
-                <td>12:50-13:40</td>
-                <td>Galleta</td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
+                <?php 
+                echo "<td>12:00-12:50</td>
+                <td>SRI</td>
+                <td>ASO</td>
+                <td>ASO</td>
+                <td>IAW</td>
+                <td>SAD</td>"
+                ?>
             </tr>
             <tr>
-                <td>13:40-14:30</td>
-                <td>Substitucion</td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
+                <?php
+                echo "<td>12:50-13:40</td>
+                <td>ASO</td>
+                <td>ASO</td>
+                <td>IAW</td>
+                <td>IAW</td>
+                <td>SAD</td>"
+                ?>
             </tr>
             <tr>
-                <td>14:30-16:00</td>
-                <td>Consistencia</td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
+                <?php 
+                echo "<td>13:40-14:30</td>
+                <td>ASX</td>
+                <td>ASO</td>
+                <td>IAW</td>
+                <td>SRI</td>
+                <td>SAD</td>"
+                ?>
             </tr>
             <tr>
-                <td>16:00-16:50</td>
-                <td>Exibicion</td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
+                <?php 
+                echo "<td>14:30-16:00</td>"?>
+                <td colspan="5" align="Center">KEBAB</td>
             </tr>
             <tr>
-                <td>16:50-17:40</td>
-                <td>Despertar</td>
+                <?php 
+                echo "<td>16:00-16:50</td>
                 <td></td>
+                <td>SRI</td>
                 <td></td>
-                <td></td>
-                <td></td>
+                <td>ASO</td>
+                <td></td>"
+                ?>
             </tr>
             <tr>
-                <td>17:40-18:30</td>
-                <td>Artista</td>
+                <?php 
+                echo "<td>16:50-17:40</td>
                 <td></td>
+                <td>SRI</td>
                 <td></td>
+                <td>ASO</td>
+                <td></td>"
+                ?>
+            </tr>
+            <tr>
+                <?php 
+                echo "<td>17:10-18:30</td>
                 <td></td>
+                <td>SRI</td>
                 <td></td>
+                <td>ASO</td>
+                <td></td>"
+                ?>
             </tr>
         </tbody>
     </table>
