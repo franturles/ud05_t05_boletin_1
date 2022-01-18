de63e9ea868d391bb0809a251395f7acfb938079
franturles <kikofran35@hotmail.com>
2022-01-18 08:59:16 +0000

    Ejercicio 7

---
+++ ejercicios_7.php
@@ -0,0 +1,10 @@
+<html>
+    <?php
+    $nombre = "Francisco Lazara";
+    $direccion = "Calle desengaño 21";
+    $telefono = "986581232";
+    echo $nombre . "</br>";
+    echo $direccion . "</br>";
+    echo $telefono . "</br>";
+    ?>
+</html>
