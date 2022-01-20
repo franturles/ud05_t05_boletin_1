15be13427c8d2836a2b2b5f75805e67327f270d9
franturles <kikofran35@hotmail.com>
2022-01-20 11:15:39 +0000

    Ejercicio corregido

--- ejercicios_8.php
+++ ejercicios_8.php
@@ -2,8 +2,8 @@
     <h1>Calculadora</h1>
     <?php 
     $Euro = "20";
-    $Pesetas = "166,3860";
-    $Resultado = $Euro * $Pesetas;
-    echo $Resultado;
+    $Conversor = "166,386";
+    $Pesetas = $Euro * $Conversor;
+    echo $Pesetas;
     ?>
 </html>
