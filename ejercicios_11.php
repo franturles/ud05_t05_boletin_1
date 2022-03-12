3f28fab145bf07d8d9bfd9129adc3b60206bf074
franturles <kikofran35@hotmail.com>
2022-03-12 17:27:45 +0000

    Solucion mia

--- ejercicios_11.php
+++ ejercicios_11.php
@@ -1,24 +1,30 @@
 <html>
-    <a>  *</a></br>
     <?php
-    for($i=1;$i <= 10; $i++){
-        for ($m = 1; $m <= 1; $m++){
+for($i=1;$i <= 9; $i++){
+    for ($m=0; $m < 1; $m++) { 
             echo "*";
-        }
-        for ($m = 1; $m <= $i*2; $m++){
-            echo "&nbsp";
-        }
-        for ($m = 1; $m <= 1; $m++){
-            echo "*";
-        }
-        for ($m = 1; $m <= 10;$m++){
-            echo "&nbsp";
-        }
-        echo '<br>';
-    }
-    for ($m = 1; $m <= 9; $m++){
+    }    
+    echo "<br>";
+    for ($k=0; $k < 1; $k++) { 
         echo "*";
+        if ($i == 9) {
+            echo "&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp *";
+        }
         echo "&nbsp";
+
     }
+
+    for ($l=0; $l < $i-1; $l++) { 
+        echo "&nbsp&nbsp&nbsp";
+    }
+    
+}
+echo "<br>";
+for($x=1;$x <= 10; $x++){
+    for ($c=0; $c < 1; $c++) { 
+            echo "&nbsp";
+            echo "*"; 
+    }
+}
     ?>
 </html>
